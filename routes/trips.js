b13bff2abe4c27d42d76fbafbca015a8df6fd553
jcj0002 <jcj0002@gmail.com>
2018-06-26 15:25:50 -0400

    finished trip update route

--- routes/trips.js
+++ routes/trips.js
@@ -1,14 +1,34 @@
-let express = require ('express')
+let express = require('express')
 let router = express.Router()
-const {TripModel} = require ('../db/schema')
+const { TripModel } = require('../db/schema')
 
 // Get trip listing
-router.get('/', function (req, res){
-    TripModel.find().then((trips)=>{
+router.get('/', function (req, res) {
+    TripModel.find().then((trips) => {
         res.send({
             trips
         })
     })
 })
+//Create new trip
+router.post('/', (req, res)=>{
+    const newTrip = new TripModel(req.body)
+    newTrip.save().then((trip)=>{
+        res.send(trip)
+    })
+})
+
+//Update trip
+
+router.put('/:id', (req, res)=>{
+TripModel.findByIdAndUpdate (req.params.id, req.body, {new:true}).then((trip)=>{
+    res.send({trip:trip})
+})
+
+})
+
+//Delete trip
+
+
 
 module.exports = router
