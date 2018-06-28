b67bc6a763d2ddec8a96c425c521efe155633106
jcj0002 <jcj0002@gmail.com>
2018-06-27 21:40:47 -0400

    completed show route for plans

--- routes/plans.js
+++ routes/plans.js
@@ -1,16 +1,27 @@
 const express = require('express')
-const router = express.Router({ mergeParams:true })
-const {TripModel, PlansModel} = require('../db/schema')
+const router = express.Router({ mergeParams: true })
+const { TripModel, PlansModel } = require('../db/schema')
 
 //Get all Travelers
-router.get('/',function (req,res) {
-TripModel.findById(req.params.tripId)
-.then((trip)=>{
-   const plans = trip.plans
-   res.send({
-      plans 
-   })
+router.get('/', function (req, res) {
+    TripModel.findById(req.params.tripId)
+        .then((trip) => {
+            const plans = trip.plans
+            res.send({
+                plans
+            })
+        })
 })
+
+//show a single traveler
+router.get('/:id', (req, res) => {
+    TripModel.findById(req.params.tripId)
+    .then((trip)=>{
+        const individualPlan = trip.plans.id(req.params.id)
+        res.send({
+            individualPlan
+        })
+    })
 })
 
 
