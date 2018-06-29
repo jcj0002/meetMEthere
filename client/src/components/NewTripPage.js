24b029dfdbc97a6562038b08ade9e7ace99f3a4f
jcj0002 <jcj0002@gmail.com>
2018-06-29 13:10:03 -0400

    completed form

--- client/src/components/NewTripPage.js
+++ client/src/components/NewTripPage.js
@@ -3,65 +3,94 @@ import axios from 'axios'
 
 class NewTripPage extends Component {
     state = {
-    name: '' ,
-    location: '',
-    departureDate: '',
-    returnDate: '',
-    description: '',
-    hotel: '',
-    image: ''
+        name: '',
+        location: '',
+        departureDate: '',
+        returnDate: '',
+        description: '',
+        hotel: '',
+        image: ''
     }
-    
-    handleChange = (event)=>{
+
+    handleChange = (event) => {
         const inputName = event.target.name
         const tripInput = event.target.value
-        this.setState({[ inputName ]:tripInput})
-       
+        this.setState({ [inputName]: tripInput })
+
     }
     handleSubmit = (event) => {
         event.preventDefault()
-        axios.post('/api/trips', this.state).then((res)=>{
+        axios.post('/api/trips', this.state).then((res) => {
             console.log(res.data)
             this.props.history.push(`/trips/${res.data._id}`)
         })
     }
-
     
+
+
     render() {
         return (
             <div>
                 <h1>Create A Trip</h1>
-                
+
                 <form onSubmit={this.handleSubmit}>
-                <input
-                placeholder="Trip Name"
-                type="text"
-                name= "name"
-                value={this.state.name}
-                onChange={this.handleChange}
-                />
-                <br/>
-                <input
-                placeholder = "Location"
-                type="text"
-                name="location"
-                value={this.state.location}
-                onChange={this.handleChange}/>
- <br/>
-                <input
-                placeholder = "Departure Date"
-                type="date"
-                name="departureDate"
-                value={this.state.departureDate}
-                onChange={this.handleChange}/>
+                    <input
+                        placeholder="Trip Name"
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input
+                        placeholder="Location"
+                        type="text"
+                        name="location"
+                        value={this.state.location}
+                        onChange={this.handleChange} />
+                    <br />
+                    <input
+                        placeholder="Description"
+                        type="description"
+                        name="description"
+                        value={this.state.description}
+                        onChange={this.handleChange} />
+                    <br />
+                    <input
+                        placeholder="Hotel Information"
+                        type="text"
+                        name="hotel"
+                        value={this.state.hotel}
+                        onChange={this.handleChange} />
+                    <br />
+                    <input
+                        placeholder="Trip Image"
+                        type="text"
+                        name="image"
+                        value={this.state.image}
+                        onChange={this.handleChange} />
+                    <br />
+                    <input
+                        placeholder="Departure Date"
+                        type="date"
+                        name="departureDate"
+                        value={this.state.departureDate}
+                        onChange={this.handleChange} />
+                        <br/>
+                    <input
+                        placeholder="Return Date"
+                        type="date"
+                        name="returnDate"
+                        value={this.state.returnDate}
+                        onChange={this.handleChange} />
 
-                
 
-                
-                
+
+
+
                 </form>
 
-                
+
             </div>
         );
     }
