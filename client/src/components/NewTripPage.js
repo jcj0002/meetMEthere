a0c8d0ce798a6b06a2ba9b62e46bc011e81fdc3f
jcj0002 <jcj0002@gmail.com>
2018-06-29 12:25:41 -0400

    created handle change and handle submit

--- client/src/components/NewTripPage.js
+++ client/src/components/NewTripPage.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class NewTripPage extends Component {
+    state = {
+    name: '' ,
+    location: '',
+    departureDate: '',
+    returnDate: '',
+    description: '',
+    hotel: '',
+    image: ''
+    }
+    
+    handleChange = (event)=>{
+        const inputName = event.target.name
+        const tripInput = event.target.value
+        this.setState({[ inputName ]:tripInput})
+       
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('/api/trips', this.state).then((res)=>{
+            console.log(res.data)
+            this.props.history.push(`/trips/${res.data._id}`)
+        })
+    }
 
     
     render() {
         return (
             <div>
                 <h1>Create A Trip</h1>
+                
+                <form onSubmit={this.handleSubmit}>
+                <input
+                placeholder="Trip Name"
+                type="text"
+                name= "name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                />
+
+                
+                
+                </form>
 
                 
             </div>
