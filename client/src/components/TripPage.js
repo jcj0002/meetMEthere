739c711c3b2f7f387654e96f286acf337174628a
jcj0002 <jcj0002@gmail.com>
2018-07-01 20:11:43 -0500

    styled trip page

--- client/src/components/TripPage.js
+++ client/src/components/TripPage.js
@@ -3,42 +3,89 @@ import axios from 'axios'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const TripContainer = styled.div`
-background-color: 	#bae1ff
-border: solid turquoise 10px;
 
 
-`
+
+
 const Title = styled.div`
 text-align: center;
+/* font-family: 'Bonbon', cursive; */
+text-shadow:2px;
+color: #6D929B;
+text-shadow: 2px 1px black;
+font-size: 35px;
+text-decoration:underline;
+
 `
 
 const TripImage = styled.div`
 .TripImage{
-    display: flex
+    display: flex;
     justify-content: center;
     width: 50vw;
+    size:30px;
    
     
 
 }
 .Container {
     border: solid white 2px;
-    box-shadow:5px 10px 18px #888888
+    box-shadow:5px 10px 18px #888888;
     background: white;
-   display: flex
-   flex-direction: row
-   flex-wrap:wrap
+   display: flex;
+   flex-direction: row;
+   flex-wrap:wrap;
    justify-content:center;
    align-items: baseline;
-   width: 50vw
+   width: 50vw;
    padding: 70px;
    margin-left: 20%;
+   border-radius: 8px;
+   
 
 
    
 }
 `
+const TripContainer = styled.div`
+background-color: 	#bae1ff;
+border: solid turquoise 10px;
+text-align: center;
+
+h2{
+    color: white;
+    -webkit-text-stroke: .5px black;
+    font-size: 30px;
+}
+h3{
+    font-size: 30px;
+    color:tan;
+ 
+}
+
+button{
+   background-color: turquoise;
+   color: white;
+   font-size: 25px;
+   padding: 10px;
+   border-radius:8px;
+    
+}
+
+.CreateButton{
+    text-align:right;
+    margin-right: 82%;
+    margin-bottom:30px;
+   
+}
+
+`
+
+
+
+
+
+
 
 
 
@@ -60,27 +107,37 @@ class TripPage extends Component {
         return (
             <TripContainer>
                 <Title>
-                <h1>meetMEthere!</h1>
+                    <h1>meetMEthere!</h1>
+
                 </Title>
-                <form>
-                    Name: <input type='text' />
-                </form>
+
                 {this.state.trips.map((trips) => {
                     return (
                         <TripImage key={trips._id}>
-                            <p>{trips.name}</p>
-                            <div>{trips.location}</div>
+                            <h2>{trips.name}</h2>
+                            
                             <div className='Container'>
+                            <h3>{trips.location}</h3>
                                 <img className='TripImage' src={trips.image} alt={trips.name} />
+                                
                                 <div>{trips.description}</div>
+                                <br/>
+                                
                             </div>
+                            <br/>
                             <Link to={`/trips/${trips._id}`}><button>VIEW</button></Link>
+                            <br />
+                           
                         </TripImage>
                     )
                 })}
+
                 <br />
-                <Link to="/trips/new"><button>START PLANNING</button></Link>
+               
+                    <Link to="/trips/new"><button className="CreateButton" >START PLANNING</button></Link>
+               
                 <br />
+
 
             </TripContainer>
         );
